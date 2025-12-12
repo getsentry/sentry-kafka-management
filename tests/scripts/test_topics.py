2dcd20320c70c2adc56f991ebd8c0d80df8d4316
Tony Le <tony.le@sentry.io>
2025-12-12 11:18:12 -0500

    Not mock list topics anymore

--- tests/scripts/test_topics.py
+++ tests/scripts/test_topics.py
@@ -7,19 +7,18 @@ from click.testing import CliRunner
 from sentry_kafka_management.scripts.topics import list_topics
 
 
-@patch("sentry_kafka_management.scripts.topics.list_topics_action")
 @patch("sentry_kafka_management.scripts.topics.get_admin_client")
-def test_list_topics(mock_get_admin: MagicMock, mock_action: MagicMock, temp_config: Path) -> None:
-    mock_topics = [
-        "topic1",
-        "topic2",
-    ]
-    mock_action.return_value = mock_topics
+def test_list_topics(mock_get_admin: MagicMock, temp_config: Path) -> None:
+    mock_client = MagicMock()
+    mock_client.list_topics.return_value.topics = {
+        "topic1": MagicMock(),
+        "topic2": MagicMock(),
+    }
+    mock_get_admin.return_value = mock_client
 
     runner = CliRunner()
     result = runner.invoke(list_topics, ["--config", str(temp_config), "--cluster", "cluster1"])
 
     assert result.exit_code == 0
-    mock_action.assert_called_once()
     parsed_output = json.loads(result.output)
-    assert parsed_output == mock_topics
+    assert parsed_output == ["topic1", "topic2"]
