44717a2553176aee3bf6b39aaed5258e65929c88
Ben McKerry <110857332+bmckerry@users.noreply.github.com>
2025-12-30 14:02:20 -0500

    feat(kafka_cli): add local action for parsing kafka-configs output

--- sentry_kafka_management/actions/local/kafka_cli.py
+++ sentry_kafka_management/actions/local/kafka_cli.py
@@ -1,1 +1,174 @@
-# TODO: add actions which run and parse output from Kafka CLI commands
+import subprocess
+from dataclasses import dataclass
+from enum import StrEnum
+from typing import Sequence
+
+
+class ConfigTypes(StrEnum):
+    """
+    Enum of allowed config types returned by `kafka-configs --describe`.
+    """
+
+    DEFAULT_CONFIG = "DEFAULT_CONFIG"
+    STATIC_BROKER_CONFIG = "STATIC_BROKER_CONFIG"
+    # Used when a dynamic config is set on a single broker
+    DYNAMIC_BROKER_CONFIG = "DYNAMIC_BROKER_CONFIG"
+    # Used when a dynamic config is set as a cluster-level default
+    DYNAMIC_DEFAULT_BROKER_CONFIG = "DYNAMIC_DEFAULT_BROKER_CONFIG"
+
+    @staticmethod
+    def values() -> set[str]:
+        """
+        Get all enum values.
+        """
+        return set(ConfigTypes._value2member_map_.keys())
+
+
+@dataclass
+class Config:
+    """
+    Represents a Config object parsed from the output of the
+    `kafka-configs` CLI.
+
+    Fields:
+        config_name: Name of the config
+        is_sensitive: Whether or not the config is sensitive
+                    (and therefore shouldn't have its value displayed)
+        active_value: The current active value of the config.
+                    Value hierarchy is: dynamic > static > default.
+        dynamic_value: Optional. The current dynamic value for the config (if one exists).
+        static_value: Optional. The current static value for the config (if one exists).
+        default_value: Optional. The current default value for the config (if one exists).
+    """
+
+    config_name: str
+    is_sensitive: bool
+    active_value: str
+    dynamic_value: str | None
+    static_value: str | None
+    default_value: str | None
+
+
+def _str_to_bool(boolstr: str) -> bool:
+    """
+    Converts the string literal `"true"` or `"false"` to
+    its boolean value.
+    Raises an error if boolstr isn't a string representation of a bool.
+    """
+    if boolstr.lower() == "true":
+        return True
+    elif boolstr.lower() == "false":
+        return False
+    else:
+        raise ValueError(f"{boolstr} is not a string representation of a bool.")
+
+
+def _str_to_dict(dictstr: str) -> dict[str, str]:
+    """
+    Converts a string representation of a dict into a dict.
+    Differs from `json.loads()` by expecting the string to be in the format:
+    `"{key:config_name=value, key:config_name=value, ...}"`
+    """
+    # if dictstr is empty, return that
+    if dictstr == "{}":
+        return {}
+    if not dictstr.startswith("{") or not dictstr.endswith("}"):
+        raise ValueError(
+            'Expected string representation of dict in the format `"{key:value, key:value, ...}"`, '
+            f"instead got {dictstr}"
+        )
+    stripped = dictstr.strip("{}")
+    split = stripped.split(", ")
+    res = {}
+    for item in split:
+        if ":" not in item or "=" not in item:
+            raise ValueError(f"Malformed item in string dict: {item}")
+        [name, value] = item.split("=", 1)
+        # get rid of the config name in the dict key
+        [name, _] = name.split(":", 1)
+        res[name] = value
+    return res
+
+
+def _run_kafka_configs_describe(broker_id: int) -> list[str]:
+    """
+    Runs `kafka-configs --entity-type brokers --describe --all` against
+    a local broker and returns the output as a list of lines.
+    Removes the first line as that doesn't contain a config.
+    """
+    # We aren't running with `shell=True`, but more validation doesn't hurt
+    if type(broker_id) is not int:
+        raise ValueError(f"{broker_id} is not a valid value for broker ID.")
+
+    command = [
+        "kafka-configs",
+        "--bootstrap-server",
+        "localhost:9092",
+        "--entity-type",
+        "brokers",
+        "--entity-name",
+        str(broker_id),
+        "--describe",
+        "--all",
+    ]
+    res = subprocess.run(command, capture_output=True, text=True)
+    res.check_returncode()
+    lines = res.stdout.split("\n")
+    if lines[0].strip() != f"All configs for broker {broker_id} are:":
+        raise ValueError(f"Got unexpected output from kafka-configs:\n{lines}")
+    return [line.strip() for line in lines[1:] if line]
+
+
+def _parse_line(line: str) -> Config:
+    """
+    Parses a single line from the output of `_run_kafka_configs` into a Config object.
+    """
+    items = line.split(" ", 2)
+    # validate the line has the expected number of items in it
+    if len(items) != 3:
+        raise ValueError(f"Config line had an unexpected number of items: {items}")
+    # extract config name and current value
+    [name, value] = items[0].split("=")
+    # extract if config is sensitive
+    is_sensitive = _str_to_bool(items[1].split("=")[1])
+    # extract dynamic/static/default values, if they exist
+    synonyms = _str_to_dict(items[2].split("=", 1)[1])
+    assert all(
+        [conf in ConfigTypes.values() for conf in synonyms.keys()]
+    ), f'Extracted invalid keys from config synonyms in line "{line}", '
+    f"expected one of {ConfigTypes.values()} but got {synonyms.keys()}"
+    return Config(
+        config_name=name,
+        active_value=value,
+        is_sensitive=is_sensitive,
+        dynamic_value=synonyms.get(ConfigTypes.DYNAMIC_BROKER_CONFIG),
+        static_value=synonyms.get(ConfigTypes.STATIC_BROKER_CONFIG),
+        default_value=synonyms.get(ConfigTypes.DEFAULT_CONFIG),
+    )
+
+
+def _parse_output(lines: Sequence[str]) -> list[Config]:
+    """
+    Takes the output from `_run_kafka_configs` and parses it into a list of Config objects.
+    """
+    res = []
+    for line in lines:
+        res.append(_parse_line(line))
+    return res
+
+
+def get_active_broker_configs(broker_id: int) -> list[Config]:
+    """
+    Runs `kafka-configs --entity-type brokers --describe --all` against
+    the local broker, and parses its output to get a list of all configs
+    (and their active values on the broker).
+
+    This differs from the `describe_broker_configs()` action as it tells you
+    both the active value for a config, but also the underlying
+    dynamic/static/default values for that config. This means we can get
+    the static value that Kafka has stored for a config even if that config has
+    a dynamic value set.
+    """
+    kafka_configs_lines = _run_kafka_configs_describe(broker_id)
+    configs = _parse_output(kafka_configs_lines)
+    return configs
---
+++ tests/actions/local/test_kafka_cli.py
@@ -0,0 +1,123 @@
+from unittest.mock import MagicMock, patch
+
+import pytest
+
+from sentry_kafka_management.actions.local.kafka_cli import (
+    Config,
+    _parse_line,
+    _parse_output,
+    _run_kafka_configs_describe,
+    _str_to_bool,
+    _str_to_dict,
+    get_active_broker_configs,
+)
+
+
+def test_str_to_bool() -> None:
+    assert _str_to_bool("true")
+    assert _str_to_bool("True")
+    assert not _str_to_bool("false")
+    assert not _str_to_bool("False")
+    with pytest.raises(ValueError):
+        _str_to_bool("foo")
+
+
+def test_str_to_dict() -> None:
+    assert _str_to_dict("{foo:fake=bar, test:fake=123}") == {"foo": "bar", "test": "123"}
+    assert _str_to_dict("{foo:bar=bin:baz}") == {"foo": "bin:baz"}
+    with pytest.raises(ValueError):
+        _str_to_dict("foo:bar")
+    with pytest.raises(ValueError):
+        _str_to_dict("test123")
+
+
+@patch("sentry_kafka_management.actions.local.kafka_cli.subprocess.run")
+def test_run_kafka_configs_describe(mock_run: MagicMock) -> None:
+    mock_run_output = MagicMock()
+    mock_run_output.check_returncode.return_value = 0
+    mock_run_output.stdout = "All configs for broker 1 are:\n    foo:bar"
+    mock_run.return_value = mock_run_output
+    assert _run_kafka_configs_describe(1) == ["foo:bar"]
+
+
+def test_parse_line() -> None:
+    regular_line = (
+        "offsets.topic.num.partitions=1 sensitive=false synonyms="
+        "{STATIC_BROKER_CONFIG:offsets.topic.num.partitions=1, "
+        "DEFAULT_CONFIG:offsets.topic.num.partitions=50}"
+    )
+    expected = Config(
+        config_name="offsets.topic.num.partitions",
+        active_value="1",
+        is_sensitive=False,
+        dynamic_value=None,
+        static_value="1",
+        default_value="50",
+    )
+    assert _parse_line(regular_line) == expected
+
+    malformed_line = "offsets.topic.num.partitions=1"
+    with pytest.raises(ValueError):
+        _parse_line(malformed_line)
+    wrong_keys = (
+        "offsets.topic.num.partitions=1 sensitive=false synonyms="
+        "{FOO:offsets.topic.num.partitions=1,"
+        "BAR:offsets.topic.num.partitions=50}"
+    )
+    with pytest.raises(AssertionError):
+        _parse_line(wrong_keys)
+
+
+def test_parse_output() -> None:
+    input = [
+        "metrics.sample.window.ms=30000 sensitive=false synonyms={DEFAULT_CONFIG:metrics.sample.window.ms=30000}",  # noqa: E501
+        "min.insync.replicas=1 sensitive=false synonyms={DEFAULT_CONFIG:min.insync.replicas=1}",
+    ]
+    expected = [
+        Config(
+            config_name="metrics.sample.window.ms",
+            is_sensitive=False,
+            active_value="30000",
+            dynamic_value=None,
+            static_value=None,
+            default_value="30000",
+        ),
+        Config(
+            config_name="min.insync.replicas",
+            is_sensitive=False,
+            active_value="1",
+            dynamic_value=None,
+            static_value=None,
+            default_value="1",
+        ),
+    ]
+    assert _parse_output(input) == expected
+
+
+@patch(
+    "sentry_kafka_management.actions.local.kafka_cli._run_kafka_configs_describe",
+    return_value=[
+        "metrics.sample.window.ms=30000 sensitive=false synonyms={DEFAULT_CONFIG:metrics.sample.window.ms=30000}",  # noqa: E501
+        "min.insync.replicas=1 sensitive=false synonyms={DEFAULT_CONFIG:min.insync.replicas=1}",
+    ],
+)
+def test_get_active_broker_configs(mock_cli: MagicMock) -> None:
+    expected = [
+        Config(
+            config_name="metrics.sample.window.ms",
+            is_sensitive=False,
+            active_value="30000",
+            dynamic_value=None,
+            static_value=None,
+            default_value="30000",
+        ),
+        Config(
+            config_name="min.insync.replicas",
+            is_sensitive=False,
+            active_value="1",
+            dynamic_value=None,
+            static_value=None,
+            default_value="1",
+        ),
+    ]
+    assert get_active_broker_configs(1) == expected
