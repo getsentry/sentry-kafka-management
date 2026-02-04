26a3deaba2fe10725c29e74a60358c9850ad6909
Ben McKerry <110857332+bmckerry@users.noreply.github.com>
2026-02-04 17:45:11 -0500

    feat: add action to run kafkactl command

---
+++ tests/actions/test_kafkactl.py
@@ -0,0 +1,72 @@
+import os
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from unittest.mock import MagicMock, patch
+
+import yaml
+
+from sentry_kafka_management.actions.kafkactl import (
+    _maybe_create_conf,
+    _setup_cluster_password,
+    run_kafkactl,
+)
+from sentry_kafka_management.brokers import YamlKafkaConfig
+from tests.conftest import DummyKafkaConfig
+
+
+def test_maybe_create_conf(temp_config: Path) -> None:
+    with TemporaryDirectory() as tmpdir:
+        kafkactl_conf = Path(tmpdir) / "kafkactl.yaml"
+        skm_conf = YamlKafkaConfig(temp_config)
+        _maybe_create_conf(kafkactl_conf, skm_conf)
+
+        expected = {
+            "contexts": {
+                "cluster1": {
+                    "brokers": ["broker1:9092", "broker2:9092"],
+                    "requesttimeout": "10s",
+                },
+                "cluster2": {
+                    "brokers": ["broker3:9092", "broker4:9092"],
+                    "requesttimeout": "10s",
+                    "sasl": {"enabled": True, "mechanism": "PLAIN", "username": "user1"},
+                },
+            }
+        }
+        with open(kafkactl_conf) as f:
+            data = yaml.safe_load(f)
+            assert data == expected
+
+
+def test_setup_cluster_password(temp_config: Path) -> None:
+    clusters = YamlKafkaConfig(temp_config)
+    _setup_cluster_password(clusters)
+    assert os.getenv("CONTEXTS_CLUSTER2_SASL_PASSWORD") == "pass1"
+
+
+@patch("sentry_kafka_management.actions.kafkactl.subprocess.run")
+@patch("sentry_kafka_management.actions.kafkactl._setup_cluster_password")
+@patch("sentry_kafka_management.actions.kafkactl._maybe_create_conf")
+def test_run_kafkactl(
+    mock_conf: MagicMock,
+    mock_setup: MagicMock,
+    mock_run: MagicMock,
+) -> None:
+    mock_run.return_value.stdout = "command_output"
+
+    kafkactl_exec = "/usr/bin/kafkactl"
+    kafkactl_conf = "/var/conf/kafkactl.yaml"
+    cli_args = ["describe", "topic", "ingest-events"]
+    output = run_kafkactl(
+        cli_args,
+        DummyKafkaConfig(),
+        Path(kafkactl_exec),
+        Path(kafkactl_conf),
+    )
+    mock_run.assert_called_once_with(
+        [kafkactl_exec, "-C", kafkactl_conf] + cli_args,
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    assert output == "command_output"
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,13 @@
 import tempfile
 from pathlib import Path
 from tempfile import TemporaryDirectory
-from typing import Generator
+from typing import Generator, Mapping
 from unittest.mock import MagicMock
 
 import pytest
 import yaml
+
+from sentry_kafka_management.brokers import ClusterConfig, KafkaConfig, TopicConfig
 
 
 @pytest.fixture
@@ -19,6 +21,7 @@ def temp_config() -> Generator[Path, None, None]:
             "sasl_mechanism": None,
             "sasl_username": None,
             "sasl_password": None,
+            "password_is_plaintext": False,
             "topics": [
                 {
                     "name": "topic1",
@@ -43,6 +46,7 @@ def temp_config() -> Generator[Path, None, None]:
             "sasl_mechanism": "PLAIN",
             "sasl_username": "user1",
             "sasl_password": "pass1",
+            "password_is_plaintext": True,
             "topics": [
                 {
                     "name": "topic3",
@@ -92,3 +96,56 @@ def temp_sasl_credentials_file() -> Generator[Path, None, None]:
     with TemporaryDirectory() as tmpdir:
         sasl_file = Path(tmpdir) / "client.properties"
         yield sasl_file
+
+
+class DummyKafkaConfig(KafkaConfig):
+    """
+    Dummy KafkaConfig object which can be used in tests.
+    """
+
+    def get_clusters(self) -> Mapping[str, ClusterConfig]:
+        return {
+            "cluster1": ClusterConfig(
+                brokers=["broker1:9092", "broker2:9092"],
+                security_protocol="PLAINTEXT",
+                sasl_mechanism=None,
+                sasl_username=None,
+                sasl_password=None,
+                password_is_plaintext=False,
+            ),
+            "cluster2": ClusterConfig(
+                brokers=["broker3:9092", "broker4:9092"],
+                security_protocol="SASL_SSL",
+                sasl_mechanism="PLAIN",
+                sasl_username="user1",
+                sasl_password="pass1",
+                password_is_plaintext=True,
+            ),
+        }
+
+    def get_topics_config(self, cluster_name: str) -> Mapping[str, TopicConfig]:
+        if cluster_name == "cluster1":
+            return {
+                "topic1": TopicConfig(
+                    partitions=3,
+                    placement={"rack": "rack1"},
+                    replication_factor=2,
+                    settings={"retention.ms": "86400000"},
+                ),
+                "topic2": TopicConfig(
+                    partitions=5,
+                    placement={"rack": "rack2"},
+                    replication_factor=3,
+                    settings={"cleanup.policy": "delete"},
+                ),
+            }
+        elif cluster_name == "cluster2":
+            return {
+                "topic3": TopicConfig(
+                    partitions=2,
+                    placement={"zone": "zone1"},
+                    replication_factor=2,
+                    settings={"compression.type": "snappy"},
+                ),
+            }
+        return {}
