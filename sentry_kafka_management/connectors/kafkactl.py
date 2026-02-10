35ddd019a804236bfc010d8f8ecd9e88db6e75f6
Ben McKerry <110857332+bmckerry@users.noreply.github.com>
2026-02-10 13:21:43 -0500

    feat: remove kafkactl

--- sentry_kafka_management/connectors/kafkactl.py
+++
@@ -1,4 +0,0 @@
-# TODO: Possibly implement a wrapper around calls to kafkactl.
-#       This may not be needed, we will see. At this point this is
-#       just an example. If we support this we may have tio wrap
-#       an ssh connection.
